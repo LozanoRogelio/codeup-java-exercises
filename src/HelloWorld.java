af8fc9beae4f55b5cc29f65c3fc00d914b765880
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-08-01 16:55:12 -0500

    Finished syntax, variables, etc.

---
+++ src/HelloWorld.java
@@ -0,0 +1,56 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("");
+
+        int myFavoriteNumber = 12;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "wings";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+        myNumber =123L;
+        System.out.println(myNumber);
+
+//        x++ adding 1 after output
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        System.out.println();
+
+//        ++x adding 1 before output
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+        int z = 4;
+        z += 5;
+        System.out.println(z);
+
+        int r = 3;
+        int y = 4;
+        y *= r;
+        System.out.println(y);
+
+        int w = 10;
+        int v = 2;
+         w /= v;
+         v -= w;
+        System.out.println(v);
+
+
+//        byte myByte = (byte) 3000;
+//        System.out.println(myByte);
+
+
+    }
+}
